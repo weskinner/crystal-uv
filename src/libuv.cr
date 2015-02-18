b4b5cb9e008e8e33bd49ad0184fa1523886ea966
William Skinner <william.skinner@chiprewards.com>
2015-02-17 23:32:43 -0600

    improve UV binding. add idle funcs

--- src/libuv.cr
+++ src/libuv.cr
@@ -1,16 +1,20 @@
 @[Link("uv")]
 lib UV
-  struct UvLoopT
-    data : Void*
-  end
-
   enum UvRunMode
     UV_RUN_DEFAULT = 0
     UV_RUN_ONCE
     UV_RUN_NOWAIT
   end
 
-  fun loop_init = uv_loop_init(loop : UvLoopT*) : Int32
-  fun loop_close = uv_loop_close(loop : UvLoopT*) : Int32
-  fun run = uv_run(loop : UvLoopT*, mode : UvRunMode) : Int32
+  struct Handle
+    data : Void*
+  end
+
+  fun loop_init = uv_loop_init(loop : Void*) : Int32
+  fun loop_close = uv_loop_close(loop : Void*) : Int32
+  fun run = uv_run(loop : Void*, mode : UvRunMode) : Int32
+  fun default_loop = uv_default_loop() : Void*
+  fun idle_init = uv_idle_init(loop : Void*, idler : Handle*) : Int32
+  fun idle_start = uv_idle_start(idler : Handle*, callback : Handle* -> ) : Int32
+  fun idle_stop = uv_idle_stop(idler : Handle*) : Int32
 end
