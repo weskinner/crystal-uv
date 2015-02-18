acf39ada3b0d9e4911bef60b69ecd24ddc98c6c2
William Skinner <william.skinner@chiprewards.com>
2015-02-17 23:25:14 -0600

    loop init and run specs

--- spec/libuv_spec.cr
+++ spec/libuv_spec.cr
@@ -1,10 +1,17 @@
-require "../src/libuv.cr"
+require "../src/libuv_simple.cr"
 require "spec"
 
 describe "Loop" do
   it "initializes" do
     loop = Pointer(UV::UvLoopT).malloc(1)
-    UV.uv_loop_init(loop).should eq 0
-    UV.uv_loop_close(loop).should eq 0
+    UV.loop_init(loop).should eq 0
+    UV.loop_close(loop).should eq 0
+  end
+
+  it "runs" do
+    loop = Pointer(UV::UvLoopT).malloc(1)
+    UV.loop_init(loop).should eq 0
+    UV.run(loop, UV::UvRunMode::UV_RUN_DEFAULT).should eq 0
+    UV.loop_close(loop).should eq 0
   end
 end
