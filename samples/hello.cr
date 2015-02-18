e01863a725e290b2fa1c31a3e1e95be43da28d20
William Skinner <william.skinner@chiprewards.com>
2015-02-17 23:32:54 -0600

    fix hello sample

--- samples/hello.cr
+++ samples/hello.cr
@@ -1,6 +1,6 @@
 require "../src/libuv.cr"
 
-loop = Pointer(UV::UvLoopT).malloc(1)
+loop = Pointer(Void).malloc(1)
 UV.loop_init(loop)
 UV.run(loop, UV::UvRunMode::UV_RUN_DEFAULT)
 puts "Now quitting."
