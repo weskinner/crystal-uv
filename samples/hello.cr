a6de7a9ab439cf12ce131e332d8119a25ae65d32
William Skinner <william.skinner@chiprewards.com>
2015-02-17 23:40:42 -0600

    add original c sample for hello

--- samples/hello.cr
+++ samples/hello.cr
@@ -1,5 +1,17 @@
 require "../src/libuv.cr"
 
+# int main() {
+#     uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+#     uv_loop_init(loop);
+# 
+#     printf("Now quitting.\n");
+#     uv_run(loop, UV_RUN_DEFAULT);
+# 
+#     uv_loop_close(loop);
+#     free(loop);
+#     return 0;
+# }
+
 loop = Pointer(Void).malloc(1)
 UV.loop_init(loop)
 UV.run(loop, UV::UvRunMode::UV_RUN_DEFAULT)
